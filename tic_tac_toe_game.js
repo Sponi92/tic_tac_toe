5a308a27a9cc6e5216e9ac4b9b9a63ac1463e247
Sponi92 <sven.sponagel@freenet.de>
2021-02-01 12:24:25 +0100

    -newfunctionality

--- tic_tac_toe_game.js
+++ tic_tac_toe_game.js
@@ -4,6 +4,7 @@
         gameboard: new Array(9),
         playerArray: new Array(2),
         pictureArray: [],
+        clickCount: 0,
         savePictures: function(){
             for (let i=0; i<5; i++){
             this.tyrion=document.createElement("img")
@@ -45,9 +46,16 @@
                         }
                         else{
                     if(item.target.textContent===""){
-                game.chooseMarker(item.target); 
-                game.cachegrid(item.target);
-                game.checkWinner();     
+                game.chooseMarkerHuman(item.target); 
+                game.cachegridHuman(item.target);
+                game.checkWinner();
+                if (game.playerCount===2 && gameBoard.clickCount<4 || game.playerCount===3 && gameBoard.clickCount<4){
+                    game.chooseMarkerPC(); 
+                    game.cacheGridPC();
+                    game.checkWinner();
+                    gameBoard.clickCount=gameBoard.clickCount+1;
+                }   
+
                     }
                 else{
                     return
@@ -59,48 +67,36 @@
         setHumanButton1: function(){
             
             gameBoard.humanButton1.addEventListener("click", event =>{
-            //let x = prompt("Put in your Name")
-            if (x===null){
-                let player1=player("No one",0,"X",true,this.jon)
-                this.playerArray[0]=(player1);
-                this.disableButtons1();
-            }
-            else{
-            let player1 = player(x,0,"X" , true,this.jon);
+
+            let player1 = player("Jon Snow ",0,"X" , true,this.jon);
             this.playerArray[0]=(player1);
             this.disableButtons1();
-            }
+
             });
         },
-        setComputerButton1: function(){
-            
-            gameBoard.computerButton1.addEventListener("click", event =>{
-            player1 = player("Computer",0, "X", false,this.jon);
-            this.playerArray[0]=(player1);
-            this.disableButtons1();
-            });
-        },
+
         setHumanButton2: function(){
             
             gameBoard.humanButton2.addEventListener("click", event =>{
-            //let x = prompt("Put in your Name") 
-            if (x===null){
-                player2=player("No one",1,"O",true,this.tyrion)
-                this.playerArray[1]=(player2);
-                this.disableButtons2();
-            }
-            else{
-            player2 = player(x,1, "O", true,this.tyrion);
+            player2 = player("Tyrion Lannister",1, "O", true,this.tyrion);
             this.playerArray[1]=(player2);
             this.disableButtons2();
-            }
         });
+        },
+
+        setComputerButton1: function(){
+            
+            gameBoard.computerButton1.addEventListener("click", event =>{
+            player1 = player("Jon Snow",2, "X", false,this.jon);
+            this.playerArray[0]=(player1);
+            this.disableButtons1();
+            });
         },
         
         setComputerButton2: function(){
             
             gameBoard.computerButton2.addEventListener("click", event =>{
-            player2 = player("computer",1, "O", false,this.tyrion);
+            player2 = player("Tyrion Lannister",3, "O", false,this.tyrion);
             player1=player(1)
             this.playerArray[1]=(player2);
             this.disableButtons2();
@@ -125,8 +121,12 @@
                         this.enableButtons();
                         gameBoard.winner.textContent=""
                         gameBoard.winner.style.visibility="hidden"
+                        game.playerCount=0,
+                        game.pictureCount=1;
+                        gameBoard.clickCount=0;
                     })
                 } )
+
             },
         enableButtons: function(){
             gameBoard.humanButton1.disabled=false;
@@ -159,35 +159,70 @@
             
         const game= {
             library: new Array(9),
-            counter: 1,
-
-
-            chooseMarker: function(x){
-                if (this.counter===1){
-                    
-                    x.appendChild(gameBoard.pictureArray[gameBoard.playerArray[0].number])
-                    this.counter=0;
-                    gameBoard.playerArray[0].number=gameBoard.playerArray[0].number+2
+            playerCount: 0,
+            pictureCount: 1,
+            chooseMarkerHuman: function(x){
+                if (this.playerCount===0){
+                    this.pictureCount=this.pictureCount-1
+                    x.appendChild(gameBoard.pictureArray[this.pictureCount])
+                    this.pictureCount=this.pictureCount+2
 
 
                 }
-                else{
-                    x.appendChild(gameBoard.pictureArray[gameBoard.playerArray[1].number])
-                    this.counter=1;
-                    gameBoard.playerArray[1].number=gameBoard.playerArray[1].number+2
-
+                else if(this.playerCount===1){
+                    this.pictureCount=this.pictureCount-1
+                    x.appendChild(gameBoard.pictureArray[this.pictureCount])
+                    this.pictureCount=this.pictureCount+2
                 }
             },
-        cachegrid: function(x){
-            if (this.counter===0){
+            chooseMarkerPC: function(){
+                if(this.playerCount===2){                  
+                    do
+                         randomNumber=this.createRandom();
+                         while(gameBoard.gridContent[randomNumber].innerHTML!=="");
+                     this.pictureCount=this.pictureCount-1
+                     gameBoard.gridContent[randomNumber].appendChild(gameBoard.pictureArray[this.pictureCount]);
+                     
+                     this.pictureCount=this.pictureCount+2
+                 }
+                 else{
+                    do
+                    randomNumber=this.createRandom();
+                    while(gameBoard.gridContent[randomNumber].innerHTML!=="");
+                this.pictureCount=this.pictureCount-1
+                gameBoard.gridContent[randomNumber].appendChild(gameBoard.pictureArray[this.pictureCount]);
+                this.pictureCount=this.pictureCount+2;
+                 }
+
+            },
+
+            createRandom: function(){
+                    return Math.floor(Math.random() * Math.floor(9));
+            },
+
+        cachegridHuman: function(x){
+            if (this.playerCount===0){
             this.library[x.dataset.index]=gameBoard.playerArray[0].marker
+            this.playerCount=gameBoard.playerArray[1].number;
 
             }
-            else{
-            this.library[x.dataset.index]=gameBoard.playerArray[1].marker
-
+            else if(this.playerCount===1){
+                this.library[x.dataset.index]=gameBoard.playerArray[1].marker 
+                this.playerCount=gameBoard.playerArray[0].number;  
             }
+
         },
+        cacheGridPC: function(){
+            if (this.playerCount===2){
+                this.library[randomNumber]=gameBoard.playerArray[0].marker
+                this.playerCount=gameBoard.playerArray[1].number;
+                }
+            else{
+                this.library[randomNumber]=gameBoard.playerArray[1].marker
+                this.playerCount=gameBoard.playerArray[0].number;
+                }
+        },
+
         checkWinner: function(){
             if
             (this.library[0]==="X" && this.library[1]==="X" && this.library[2]==="X" || 
@@ -199,8 +234,9 @@
             this.library[0]==="X" && this.library[4]==="X" && this.library[8]==="X" ||   
             this.library[2]==="X" && this.library[4]==="X" && this.library[6]==="X" 
             ){
-                gameBoard.winner.textContent="Jon Snow Wins!"
+                gameBoard.winner.textContent=gameBoard.playerArray[0].name+ " Wins!"
                 gameBoard.winner.style.visibility="visible"
+                this.counter=0
             }
             else if
             (this.library[0]==="O" && this.library[1]==="O" && this.library[2]==="O" || 
@@ -212,8 +248,9 @@
             this.library[0]==="O" && this.library[4]==="O" && this.library[8]==="O" ||   
             this.library[2]==="O" && this.library[4]==="O" && this.library[6]==="O"
             ){
-                gameBoard.winner.textContent="Tyrion Lannister Wins!";
+                gameBoard.winner.textContent=gameBoard.playerArray[1].name+ " Wins!";
                 gameBoard.winner.style.visibility="visible";
+                this.counter=1;
             }
             else {
                 for (let i=0;i<this.library.length;i++){
@@ -223,6 +260,7 @@
                     }
                     gameBoard.winner.textContent="Draw"
                     gameBoard.winner.style.visibility="visible"
+                    this.counter=1;
                 }
              
             }
